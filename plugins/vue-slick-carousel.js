21fbcb186ff8626712c3ddfc79b2a83d51b2bcfc
Andelko Cvjetkovic <cvjetkovic.andjelko@edu.fit.ba>
2021-05-22 19:40:52 +0200

    Webpack chunks

--- plugins/vue-slick-carousel.js
+++ plugins/vue-slick-carousel.js
@@ -1,8 +1,8 @@
 import Vue from "vue";
-import VueSlickCarousel from "vue-slick-carousel";
+// import VueSlickCarousel from "vue-slick-carousel";
 
 // For Lazy Loading webpack
-// var VueSlickCarousel = () =>
-//   import(/* webpackChunkName: "vue-slick-carousel" */ "vue-slick-carousel");
+var VueSlickCarousel = () =>
+  import(/* webpackChunkName: "vue-slick-carousel" */ "vue-slick-carousel");
 
 Vue.component("VueSlickCarousel", VueSlickCarousel);
